3f90b7698b574c827c6007e131f5ec481f171c53
LeHieu <ledinhhieu.936@gmail.com>
2021-11-08 00:59:57 +0700

    save

--- src/Components/Show.jsx
+++ src/Components/Show.jsx
@@ -6,8 +6,8 @@ function Show(props) {
         <div style={{
             marginTop: '50px'
         }}>
-            <h2>Ho ten:{props.student.name}</h2>
-            <h2>Tuoi: {props.student.age}</h2>
+            <h2>Name:{props.student.name}</h2>
+            <h2>Age: {props.student.age}</h2>
         </div>
     );
 }
